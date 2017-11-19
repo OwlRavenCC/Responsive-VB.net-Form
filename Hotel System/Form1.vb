775f0839e55fd8e14a35f28aafa1ce42f149bf10
SpookyRaven <31839360+SpookyRaven@users.noreply.github.com>
2017-11-19 00:06:45 -0800

    Add files via upload
    Alpha design

---
+++ Hotel System/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub Add_btn_Click(sender As Object, e As EventArgs) Handles Add_btn.Click
+        Title_label.Text = "REGISTRATION FORM"
+        Action_btn.Text = "REGISTER"
+
+    End Sub
+
+    Private Sub Update_btn_Click(sender As Object, e As EventArgs) Handles Update_btn.Click
+        Title_label.Text = "UPDATE FORM"
+        Action_btn.Text = "UPDATE"
+    End Sub
+
+    Private Sub Delete_btn_Click(sender As Object, e As EventArgs) Handles Delete_btn.Click
+        Title_label.Text = "DELETE FORM"
+        Action_btn.Text = "DELETE"
+    End Sub
+
+    Private Sub Action_btn_Click(sender As Object, e As EventArgs) Handles Action_btn.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Call connection()
+
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
+
+    End Sub
+End Class
---
+++ Hotel System/hotel_module.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.OleDb
+Module hotel_module
+
+    Public cn As New OleDb.OleDbConnection
+    Public cm As New OleDb.OleDbCommand
+    Public dr As OleDbDataReader
+
+    Public Sub connection()
+        cn = New OleDb.OleDbConnection
+        With cn
+            .ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & Application.StartupPath & "\database\hotel_db.accdb"
+            .Open()
+        End With
+    End Sub
+
+    Public Sub AddCustomer()
+        Try
+            cm = New OleDb.OleDbCommand
+            With cm
+                .Connection = cn
+                .CommandType = CommandType.Text
+                .CommandText = "INSERT INTO tblStudent (Snum,Sname,Scourse,Ssection) VALUES (@Snum,@Sname,@Scourse,@Ssection)"
+
+                .Parameters.Add(New System.Data.OleDb.OleDbParameter("@Snum", System.Data.OleDb.OleDbType.VarChar, 255, Me.txtSearch.Text))
+                .Parameters.Add(New System.Data.OleDb.OleDbParameter("@Sname", System.Data.OleDb.OleDbType.VarChar, 255, Me.txtName.Text))
+                .Parameters.Add(New System.Data.OleDb.OleDbParameter("@Scourse", System.Data.OleDb.OleDbType.VarChar, 255, Me.txtCourse.Text))
+                .Parameters.Add(New System.Data.OleDb.OleDbParameter("@Ssection", System.Data.OleDb.OleDbType.VarChar, 255, Me.txtSection.Text))
+
+
+                ' RUN THE COMMAND
+                cm.Parameters("@Snum").Value = Me.txtSearch.Text
+                cm.Parameters("@Sname").Value = Me.txtName.Text
+                cm.Parameters("@Scourse").Value = Me.txtCourse.Text
+                cm.Parameters("@Ssection").Value = Me.txtSection.Text
+
+
+                cm.ExecuteNonQuery()
+                MsgBox("Record saved.", MsgBoxStyle.Information)
+                Me.txtCourse.Text = ""
+                Me.txtName.Text = ""
+                Me.txtSearch.Text = ""
+                Me.txtSection.Text = ""
+                Exit Sub
+            End With
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+
+End Module
